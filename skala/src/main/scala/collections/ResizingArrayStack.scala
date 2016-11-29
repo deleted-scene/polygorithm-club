7abc37500aafdcaff38f3c771b04644e742a4349
Ziya SARIKAYA <ziya.sarikaya@here.com>
2016-11-29 18:20:47 +0100

    typo

--- skala/src/main/scala/collections/ResizingArrayStack.scala
+++ skala/src/main/scala/collections/ResizingArrayStack.scala
@@ -1,9 +1,8 @@
-package collections
+import scala.reflect.ClassTag
 
+case class ResizingArrayStack[T : ClassTag](){
 
-case class ResizingArrayStack[T](){
-
-  private var array: Array[Object] = new Array[Object](2)
+  private var array: Array[T] = Array.ofDim[T](2)
   private var N:Int          = 0
 
   def isEmpty(): Boolean = N ==  0
@@ -12,29 +11,30 @@ case class ResizingArrayStack[T](){
   def resize(capacity: Int) = {
     assert(capacity > N)
 
-    val temp = new Array[Object](capacity)
-    //for(i:Int <- array.indices ) temp(i) = array(i)
-    System.arraycopy(array,0, temp,0,capacity)
+    val temp = Array.ofDim[T](capacity)
+    for(i:Int <- array.indices ) temp(i) = array(i)
     array = temp
   }
 
   def push(item: T): Unit ={
     if (array.length == N) resize( 2 * array.length)
-    N = N + 1
-    array(N) = item.asInstanceOf[Object]
+
+    array(N) = item
+    N += 1
   }
 
   def pop(): T ={
     if (isEmpty()) throw new NoSuchElementException("Stack is empty.")
 
-    val item = array(N - 1)
-    array(N-1) = null
-    N = N -1
+    val item = array(N-1)
 
+    array(N-1) = _:T //to avoid loitering
+
+    N -= 1
     //resize if necessary
     if(N > 0  && N == array.length /4 ) resize(array.length /4 )
 
-    item.asInstanceOf[T]
+    item
   }
 
 }
