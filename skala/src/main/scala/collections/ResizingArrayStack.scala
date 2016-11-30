e5346169e60548efa028141ba5b2965087358d09
Ziya SARIKAYA <ziya.sarikaya@here.com>
2016-11-30 09:48:39 +0100

    perfom formatting

--- skala/src/main/scala/collections/ResizingArrayStack.scala
+++ skala/src/main/scala/collections/ResizingArrayStack.scala
@@ -1,40 +1,41 @@
 import scala.reflect.ClassTag
 
-case class ResizingArrayStack[T : ClassTag](){
-  
+case class ResizingArrayStack[T: ClassTag]() {
+
   private var array: Array[T] = Array.ofDim[T](2)
-  private var N:Int           = 0
+  private var N: Int = 0
 
-  def isEmpty(): Boolean      = N ==  0
-  def size (): Integer        = N
-
-  private def _resize(capacity: Int) = {
-    assert(capacity > N)
-
-    val temp = Array.ofDim[T](capacity)
-    for(i:Int <- array.indices ) temp(i) = array(i)
-    array = temp
-  }
+  def size(): Integer = N
 
   def push(item: T): Unit = {
-    if (array.length == N) _resize( 2 * array.length)
+    if (array.length == N) _resize(2 * array.length)
 
     array(N) = item
     N += 1
   }
 
-  def pop(): T ={
+  private def _resize(capacity: Int) = {
+    assert(capacity > N)
+
+    val temp = Array.ofDim[T](capacity)
+    for (i: Int <- array.indices) temp(i) = array(i)
+    array = temp
+  }
+
+  def pop(): T = {
     if (isEmpty()) throw new NoSuchElementException("Stack is empty.")
 
-    val item = array(N-1)
+    val item = array(N - 1)
 
-    array(N-1) = _:T //to avoid loitering
+    array(N - 1) = _: T //to avoid loitering
 
     N -= 1
     //resize if necessary
-    if(N > 0  && N == array.length / 4 ) _resize(array.length / 4 )
+    if (N > 0 && N == array.length / 4) _resize(array.length / 4)
 
     item
   }
-  
+
+  def isEmpty(): Boolean = N == 0
+
 }
--- skala/src/test/scala/ResizingArrayStackTest.scala
+++ skala/src/test/scala/ResizingArrayStackTest.scala
@@ -1,19 +1,19 @@
 import org.scalatest._
 
-class ResizingArrayStackTest extends FlatSpec with Matchers{
+class ResizingArrayStackTest extends FlatSpec with Matchers {
 
   "A ResizingArrayStack" should "pop values in last-in-first-out order" in {
     val stack = new ResizingArrayStack[Int]
     stack.push(56)
     stack.push(66)
-    stack.pop() should be (66)
-    stack.pop() should be (56)
+    stack.pop() should be(66)
+    stack.pop() should be(56)
   }
 
 
   it should "throw NoSuchElementException if an empty stack is popped" in {
     val emptyStack = new ResizingArrayStack[Int]
-    a [NoSuchElementException] should be thrownBy {
+    a[NoSuchElementException] should be thrownBy {
       emptyStack.pop()
     }
   }
